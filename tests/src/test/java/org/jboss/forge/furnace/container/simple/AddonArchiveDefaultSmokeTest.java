b5caa96360d2b0f7bea741ea8edf60bbca1f6352
George Gastaldi <gegastaldi@gmail.com>
2015-07-14 18:45:09 -0300

    FURNACE-58: Added test for simple container

---
+++ tests/src/test/java/org/jboss/forge/furnace/container/simple/AddonArchiveDefaultSmokeTest.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.furnace.container.simple;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.furnace.Furnace;
+import org.jboss.forge.furnace.container.simple.lifecycle.SimpleContainer;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Smoke test for tests without a {@link Deployment} method
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class AddonArchiveDefaultSmokeTest
+{
+   @Test
+   public void test()
+   {
+      Furnace furnace = SimpleContainer.getFurnace(getClass().getClassLoader());
+      Assert.assertThat(furnace, notNullValue());
+   }
+
+}
